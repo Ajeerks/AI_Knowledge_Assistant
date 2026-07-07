9372be9e9f027e6b8d99c6208fdc4292eb5c78e1
Ajeer KS <ajeerkunnel@gmail.com>
2026-07-07 22:01:50 +0530

    Fix Neon database connection

--- app/database.py
+++ app/database.py
@@ -6,13 +6,10 @@ import os
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-
-engine = create_engine(DATABASE_URL)
-
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+    pool_recycle=300,
 )
 
 Base = declarative_base()
