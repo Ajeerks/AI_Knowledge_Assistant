4d3bd281b480dd4dbc5b48d33b460887fa8c0120
Ajeer KS <ajeerkunnel@gmail.com>
2026-07-07 22:18:44 +0530

    Restore SessionLocal

--- app/database.py
+++ app/database.py
@@ -6,10 +6,17 @@ import os
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+
 engine = create_engine(
     DATABASE_URL,
     pool_pre_ping=True,
     pool_recycle=300,
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
 )
 
 Base = declarative_base()
